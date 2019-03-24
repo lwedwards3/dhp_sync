9b2ea87007492422e30620a48121908b76018524
lwedwards3 <lwedwards3@gmail.com>
2019-03-23 21:31:31 -0400

    Test commit

--- vpr_sync_test.py
+++ vpr_sync_test.py
@@ -5,7 +5,7 @@ vprs = vpr_sync.VPRSync(auto_mode=False)
 
 
 vprs._get_mc_requests()
-#vprs._update_requests_from_file()
+vprs._update_requests_from_file()
 
 #vprs.sync_with_wl()
 #vprs.send_member_emails()
